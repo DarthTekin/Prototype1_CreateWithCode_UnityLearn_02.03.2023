82d6ed4422f1d748a024cacb790021a2d35eca4d
DarthTekin <tekinteks@gmail.com>
2023-03-07 23:53:07 +0300

    Input Manager class Input.GetAxis() methot is tested in vertical parameter.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     public float turnSpeed;
     public float horizontalInput;
+    public float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -21,10 +22,11 @@ public class PlayerController : MonoBehaviour
         //transform.Translate(0, 0, 1);
         //transform.Translate(Vector3.forward);
         //transform.Translate(Vector3.forward * Time.deltaTime * 20);
-        
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        
+
         horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
